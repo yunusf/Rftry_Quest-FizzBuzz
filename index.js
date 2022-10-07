d12f791f8a99c5142d77b2df7b0dd2df2b430e9b
yunusf <yunusf42@gmail.com>
2022-10-07 19:52:40 +0700

    First Make Code

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-function prosesFizzBuzz() {
-    const id_angka = document.getElementById("n_angka").value;
-    let id_t_angka = document.getElementById("t_angka");
-    let id_t_hasil = document.getElementById("t_hasil");
-    let arrHasil = [];
+var arrHasil = [];
 
-    for(i = 1; i <= id_angka; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            arrHasil.push(` fizzbuzz(${i})`);
-        } else if (i % 5 == 0) {
-            arrHasil.push(` buzz(${i})`);
-        } else if (i % 3 == 0) {
-            arrHasil.push(` fizz(${i})`);
-        } else {
-            arrHasil.push(` ${i}`);
-        }
-    }
+function prosesMinMax() {
+    let id_angka = document.getElementById("n_angka").value;
+    let id_t_angka = document.getElementById("t_angka");
+    let id_t_hasilbesar = document.getElementById("t_hasil_besar");
+    let id_t_hasilkecil = document.getElementById("t_hasil_kecil");
+        
+    arrHasil.push(id_angka);
+    id_t_angka.innerText = arrHasil;
+
+    nilai_max = Math.max(...arrHasil);    
+    nilai_min = Math.min(...arrHasil);
     
-    id_t_angka.innerText = id_angka;
-    id_t_hasil.innerText = arrHasil;
+    id_t_hasilbesar.innerText = nilai_max;
+    id_t_hasilkecil.innerText = nilai_min;
 }
